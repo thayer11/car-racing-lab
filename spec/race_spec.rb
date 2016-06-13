bae307fa8f34a96a226dd3be07454a4637e25e44
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-06-13 12:49:05 -0700

    adds example solutions

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -2,16 +2,29 @@ require 'spec_helper'
 
 RSpec.describe Race do
   describe '::new' do
-    it 'instantiates two cars'
-    it 'accelerates the cars to random speeds'
+    it 'instantiates two cars' do
+      expect(race.cars.length).to eq 2
+    end 
+    it 'accelerates the cars to random speeds' do
+      expect(race.cars.first.speed).to_not eq 0
+      expect(race.cars.last.speed).to_not eq 0
+    end
   end
   describe '#winner' do
-    it 'returns the winner'
-    it 'is not the loser'
+    it 'returns the winner' do
+      expect(race.winner).to be_a Car
+    end
+    it 'is not the loser' do
+      expect(race.winner).not_to eq race.loser
+    end
   end
 
   describe '#loser' do
-    it 'returns the loser'
-    it 'is no the winner'
+    it 'returns the loser' do
+      expect(race.loser).to be_a Car
+    end
+    it 'is not the winner' do
+      expect(race.loser).not_to eq race.winner
+    end
   end
 end
