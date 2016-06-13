b9cd05ccfa970d57bc9b9e122e6483ce14245906
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-06-13 12:44:43 -0700

    adds readme

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -3,23 +3,21 @@ require 'spec_helper'
 RSpec.describe Car do
   let(:car) { Car.new }
 
-  describe 'Attributes' do
+  describe '::new' do
+    it 'sets the speed to 0' do
+      expect(car.speed).to eq 0
+    end
+  end
+
+  describe '#speed' do
     it { is_expected.to respond_to :speed }
   end
 
-  describe 'Methods' do
-    describe '#accelerate' do
-      before { car.accelerate(20) }
+  describe '#accelerate' do
+    before { car.accelerate(20) }
 
-      it 'accelerates by a given speed' do
-        expect(car.speed).to eq 20
-      end
-    end
-
-    describe '#initialize' do
-      it 'sets the speed to 0' do
-        expect(car.speed).to eq 0
-      end
+    it 'accelerates by a given speed' do
+      expect(car.speed).to eq 20
     end
   end
 end
