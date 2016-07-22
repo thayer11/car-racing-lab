b51118217219d2cb3738189ce75a2ae96186126e
Zeb Tim Girouard <ZebGirouard@users.noreply.github.com>
2016-07-22 12:57:54 -0600

    Using ruby boolean convention

--- lib/race.rb
+++ lib/race.rb
@@ -17,12 +17,12 @@ class Race
   end
 
   # declare a winner
-  def winner
+  def winner?
     # winner is the car with the greatest speed
     cars[0].speed > cars[1].speed ? cars[0] : cars[1]
   end
 
-  def loser
+  def loser?
     # loser is the car that's not the winner
     winner == cars[0] ? cars[1] : cars[0]
   end
