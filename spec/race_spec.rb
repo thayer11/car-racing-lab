117e7faa80260ebc1124676ea6b775e576a42689
thayer11 <holly.coddington@gmail.com>
2016-07-25 17:16:52 -0600

    no_tests_pending

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -1,17 +1,33 @@
 require 'spec_helper'
 
 RSpec.describe Race do
-  describe '::new' do
-    it 'instantiates two cars'
-    it 'accelerates the cars to random speeds'
+  subject(:race) {Race.new}
+    
+    describe '::new' do
+    it 'instantiates two cars' do 
+    expect(race.cars.length).to eq 2
   end
+    it 'accelerates the cars to random speeds' do 
+      expect(race.cars.first.speed).to_not eq 0
+      expect(race.cars.last.speed).to_not eq 0
+    end
+  end
+  
   describe '#winner' do
-    it 'returns the winner'
-    it 'is not the loser'
-  end
-
+    it 'returns the winner' do 
+    expect(race.winner.speed > race.loser.speed)
+    end 
+    it 'is not the loser' do 
+    expect(race.winner.speed != race.loser.speed)
+  end 
+end
   describe '#loser' do
-    it 'returns the loser'
-    it 'is not the winner'
+    it 'returns the loser' do 
+    expect(race.loser != race.winner)
+    end 
+    it 'is not the winner' do 
+    expect (race.loser != race.winner)
+    end
   end
 end
+
